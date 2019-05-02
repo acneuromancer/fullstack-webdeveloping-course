5b3e6452b4968a6ee868df36cb45aff6b5c47d75
acneuromancer <dokukodo108@gmail.com>
2019-05-02 17:42:43 +0200

    Continued and finished the Bitcoin-Ticker example project.

--- NodeJS/Bitcoin-Ticker/index.js
+++ NodeJS/Bitcoin-Ticker/index.js
@@ -16,10 +16,34 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body.crypto);
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+  var amount = req.body.amount;
 
-  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body) {
-    console.log(response.statusCode);
+  console.log(crypto);
+  console.log(fiat);
+  console.log(amount );
+
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+
+  request(options, function(error, response, body) {
     console.log(body);
+    var data = JSON.parse(body);
+    var price = data.price;
+    console.log(price);
+
+    var currentDate = data.time;
+
+    res.write("<p>The current date is " + currentDate + "</p>");
+    res.write("<h1>" + amount + " " + crypto + " is currently worth " + price + " " + fiat + "</h1>");
+    res.send();
   });
 });
