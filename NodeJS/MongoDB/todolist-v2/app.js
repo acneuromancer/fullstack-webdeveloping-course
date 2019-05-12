728101bd27b606b94dd1c32b56a3a77f5940d376
acneuromancer <dokukodo108@gmail.com>
2019-05-12 15:06:46 +0200

    Finish todolist-v2 project, which is deployed on Heroku, and uses a mongodb on Atlas.

--- NodeJS/MongoDB/todolist-v2/app.js
+++ NodeJS/MongoDB/todolist-v2/app.js
@@ -15,9 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27018/todolistDB", {
+// mongoose.connect("mongodb://127.0.0.1:27018/todolistDB", {
+//   useNewUrlParser: true
+// });
+
+mongoose.connect("mongodb+srv://<user:password>@cluster0-ch42r.mongodb.net/todolistDB", {
   useNewUrlParser: true
-});
+})
 
 const itemsSchema = {
   name: String
@@ -139,6 +143,10 @@ app.get("/about", function(req, res){
 });
 
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
